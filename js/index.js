17b2e0968633ed377c8163905d3b294f5568b6e9
gaurav-me <gsharma.nus@gmail.com>
2022-03-26 00:47:35 +0800

    ⚡️Update log params incl DV2 homepage time

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-function onClickCupcakesSideMenu(eventName, additionalInfoObj) {
-    w3_close();
-    logEventUtil(eventName, additionalInfoObj);
+function onClickCupcakesSideMenu(e, eventName, additionalInfoObj) {
+  e.preventDefault();
+  const startHomepage = window.sessionStorage.getItem('startHomepage');
+  const homepageTime = Math.ceil(
+    new Date().getTime() - JSON.parse(startHomepage),
+  );
+  sessionStorage.removeItem('startHomepage');
+
+  w3_close();
+  logEventUtil(eventName, { DV2: homepageTime, ...additionalInfoObj });
+
+  const href = e.currentTarget.getAttribute('href');
+  window.location = href;
 }
--- js/utils/util.js
+++ js/utils/util.js
@@ -1,12 +1,16 @@
 const logEventUtil = (eventName, additionalInfoObj) => {
   const searchParams = getSearchParams();
+  const arrangement = searchParams.get('arrangement');
+  const ivDetailObj = getIVDetails(arrangement);
 
   loggingjs.logEvent(null, eventName, {
     eventName: eventName,
+    // {unique_id, trial_id, iv1_level, iv2_level, iv3_level, DV2, DV3}
     info: {
-      input: searchParams.get('input'),
+      trial_id: searchParams.get('trial_id'),
       unique_id: searchParams.get('unique_id'),
-      interface: 'new',
+      arrangement,
+      ...ivDetailObj,
       ...additionalInfoObj,
     },
   });
@@ -42,4 +46,47 @@ const getSearchParams = () => {
   }
 
   return searchParams;
+};
+
+const getIVDetails = (arr) => {
+  switch (arr) {
+    case '1':
+      return {
+        iv1_level: 'fixed-sidebar',
+        iv2_level: 'no-cat-sidebar',
+        iv3_level: '6-cupcakes',
+      };
+    case '2':
+      return {
+        iv1_level: 'fixed-sidebar',
+        iv2_level: 'no-cat-sidebar',
+        iv3_level: '3-cupcakes',
+      };
+    case '3':
+      return {
+        iv1_level: 'fixed-sidebar',
+        iv2_level: 'cat-sidebar',
+        iv3_level: '6-cupcakes',
+      };
+    case '4':
+      return {
+        iv1_level: 'fixed-sidebar',
+        iv2_level: 'cat-sidebar',
+        iv3_level: '3-cupcakes',
+      };
+    case '5':
+      return {
+        iv1_level: 'fixed-sidebar',
+        iv2_level: 'sub-cat-sidebar',
+        iv3_level: '6-cupcakes',
+      };
+    case '6':
+      return {
+        iv1_level: 'fixed-sidebar',
+        iv2_level: 'sub-cat-sidebar',
+        iv3_level: '3-cupcakes',
+      };
+    default:
+      return {};
+  }
 };
